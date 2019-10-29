da19ea6308b5af4d2140f4bac7e8c8c6529c9d36
MiTPMaPT <45069390+MiTPMaPT@users.noreply.github.com>
2019-10-29 13:11:46 +0000

    Add files via upload

---
+++ lab_05/ex12_lab.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "enter first number:";
+    int x = 0;
+    std::cin >> x;
+
+    if (x > 0) {
+        std::cout << "first number is bigger then zero\n";
+    }
+
+    std::cout << "enter second number:";
+    int x = 0;
+    std::cin >> x;
+
+    if (x > 0) {
+        std::cout << "second number is bigger then zero too\n";
+    }
+
+}
