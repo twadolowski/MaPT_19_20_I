a74df357e54d173c0a36050de4e6dbbd89d63053
redmelao <56031087+redmelao@users.noreply.github.com>
2019-11-26 15:59:47 +0100

    Add files via upload

---
+++ ex9.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+float func (float a, float b, float c)
+{
+return void ;
+}
+
+int main() {
+a=10;
+b=20;
+c=30;
+if (func(a) > func(b)){
+cout<< a << endl;
+} else if (func(){
+
+
+
+
+// incomplete
