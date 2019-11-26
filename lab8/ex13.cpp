2eae873cec876c505c1be8d38975d58dd5aace4e
KPawlica <56031138+KPawlica@users.noreply.github.com>
2019-11-26 15:48:27 +0100

    Add files via upload

---
+++ lab8/ex13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct product{
+    float weight;
+    float price;
+};
+
+int main{
+    product.tab[10]{
+    for(int i=0;i<10;i++){
+    tab[i].weight=rand();
+    tab[i].price=rand();
+    }
+    cout << tab[i].price << endl;
+    cout << tab[i].weight << endl;
+
+    }
+    }
+
+return 0;
